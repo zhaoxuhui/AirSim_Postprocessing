fb1b28096a39ba055f82543e80c836da3d573f2b
xuhui <zhaoxuhui1993@qq.com>
2021-12-06 17:28:03 +0800

    Add files via upload

---
+++ weather_fog.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import airsim
+
+if __name__ == '__main__':
+    # 连接到AirSim
+    client = airsim.VehicleClient()
+    client.confirmConnection()
+
+    # 设置打开天气控制
+    client.simEnableWeather(True)
+
+    # 范围为0到1，1表示最强
+    client.simSetWeatherParameter(airsim.WeatherParameter.Fog, 1)
